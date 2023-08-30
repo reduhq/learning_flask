941f9bc12129a3660b2931345c19153c4ae4173b
Rey Halsall <reyeduardohalsallquintero8@gmail.com>
2023-08-30 04:04:04 -0600

    Finishing my CRUD

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,8 @@
 from flask import Flask
+import secrets
+
 app = Flask(__name__)
+app.config["SECRET_KEY"] = secrets.token_hex()
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,28 @@
+from flask import render_template, request, session, redirect, url_for
+
 from app import app
 
 @app.route("/")
 def home():
-    return 'Home'
+    return render_template('home.html')
 
-@app.route("/create_list")
+@app.route("/create_list", methods=['GET', 'POST'])
 def create_list():
-    return "Create List"
+    if request.method == 'GET':
+        variable = 'reduhq'
+        return render_template('create_list.html', var=variable)
+    if request.method == 'POST':
+        data = request.form
+        name = data["nombre"]
+        quantity = data["cantidad"]
+        session[name] = quantity
+        return redirect(url_for('create_list'))
 
 @app.route("/read_list")
 def read_list():
-    return "Read List"
+    return render_template("read_list.html")
 
 @app.route("/delete_list")
 def delete_list():
-    return "Delete List"
+    session.clear()
+    return redirect(url_for('home'))
